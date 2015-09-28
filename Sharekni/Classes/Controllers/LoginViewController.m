2f3a0e4fdfdf0f61c0929d48219599170fa09e6f
Ahmed Askar <a.3askr@gmail.com>
2015-09-28 21:01:31 +0200

    update login

--- Sharekni/Classes/Controllers/LoginViewController.m
+++ Sharekni/Classes/Controllers/LoginViewController.m
@@ -9,6 +9,9 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()<UITextFieldDelegate>
+{
+    float animatedDistance ;
+}
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
@@ -34,12 +37,96 @@
 }
 
 #pragma TextFieldDelegate
-- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+#pragma mark - TextFieldDelegate
+
+// This code handles the scrolling when tabbing through infput fields
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 220;
+static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 140;
+//when clicking the return button in the keybaord
+- (BOOL)textFieldShouldEndEditing:(UITextField*)textField
+{
+    return [self textSouldEndEditing];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField*)textField
+{
+    [textField  resignFirstResponder];
     return YES;
 }
-- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    CGRect textFieldRect = [self.view.window convertRect:textField.bounds fromView:textField];
+    [self textDidBeginEditing:textFieldRect];
+}
+
+- (void)textDidBeginEditing:(CGRect)textRect
+{
+    CGRect viewRect = [self.view.window convertRect:self.view.bounds fromView:self.view];
+    CGFloat midline = textRect.origin.y + 0.5 * textRect.size.height;
+    CGFloat numerator = midline - viewRect.origin.y - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    CGFloat denominator = (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION) * viewRect.size.height;
+    CGFloat heightFraction = numerator / denominator;
+    if (heightFraction < 0.0)
+    {
+        heightFraction = 0.0;
+    }
+    else if (heightFraction > 1.0)
+    {
+        heightFraction = 1.0;
+    }
+    UIInterfaceOrientation orientation =
+    [[UIApplication sharedApplication] statusBarOrientation];
+    if (orientation == UIInterfaceOrientationPortrait ||
+        orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    }
+    else
+    {
+        animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+    }
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y -= animatedDistance;
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    [self.view setFrame:viewFrame];
+    [UIView commitAnimations];
+}
+
+- (BOOL)textSouldEndEditing
+{
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += animatedDistance;
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    [self.view setFrame:viewFrame];
+    [UIView commitAnimations];
+    
     return YES;
 }
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    for (UIView* view in self.view.subviews) {
+        for (UIGestureRecognizer* recognizer in view.gestureRecognizers) {
+            [recognizer addTarget:self action:@selector(touchEvent:)];
+        }
+        
+        [self.view endEditing:YES];
+    }
+}
+
+- (void)touchEvent:(id)sender
+{
+    
+}
+
 
 
 @end
